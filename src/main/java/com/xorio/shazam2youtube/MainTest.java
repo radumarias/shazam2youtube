60e3de13e323e09b5b06659e4de7e634e417a819
Radu Marias <radumarias@gmail.com>
2014-12-05 18:30:28 +0200

    Initial commit.

---
+++ src/main/java/com/xorio/shazam2youtube/MainTest.java
@@ -0,0 +1,13 @@
+package com.xorio.shazam2youtube;
+
+/**
+ * User: radu
+ * Date: 12/5/14
+ * Time: 6:03 PM
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
